0f289f73a95fdb2d0d0c214473795c21151d0a4c
Christian Rimondi <christian.rimondi@gmail.com>
2022-01-24 14:01:53 +0100

    Fix ruby 2.7 method deprecation warnings
    ```
    warning: URI.escape is obsolete
    ```
    
    This patch is a port of kreeti/kt-paperclip@2f093f9

--- lib/paperclip/io_adapters/http_url_proxy_adapter.rb
+++ lib/paperclip/io_adapters/http_url_proxy_adapter.rb
@@ -9,7 +9,7 @@ module Paperclip
     REGEXP = /\Ahttps?:\/\//
 
     def initialize(target, options = {})
-      super(URI(URI.escape(target)), options)
+      super(URI(Paperlip::UrlGenerator.escape(target)), options)
     end
   end
 end
